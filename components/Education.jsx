1dbe7d6a0c8d4ee571d9118264cffe7a1925dfe4
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2022-12-21 13:39:43 -0600

    styled education page

--- components/Education.jsx
+++ components/Education.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 
-const Training = () => {
+const Education = () => {
   return (
     <div className='w-full md:h-screen p-2 flex items-center py-16'>
         <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
           <div className='col-span-2'>
-            <p>Education</p>
-            <h2>Codeup</h2>
-            <p>Jul 2020 - Jan 2023</p>
-            <p>
+            <p className='uppercase text-xl tracking-widest'>Education</p>
+            <h2 className='py-4'>Codeup - Dallas, TX</h2>
+            <p className='py-2 text-gray-600'>Jul 2020 - Jan 2023</p>
+            <p className='py-2 text-gray-600'>
                 A Fully immersive, project-based, and intensive 20-week program that provides individuals with 670 hours of expert instruction and mentorship in software development.
             </p>
           </div>
-          <div>
-            <img src="assets/CodeupLogo.png" alt="codeup logo" />
+          <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+            <img className='p-2 rounded-xl' src="assets/CodeupLogo.png" alt="codeup logo" />
           </div>
         </div>
     </div>
   )
 }
 
-export default Training
+export default Education
