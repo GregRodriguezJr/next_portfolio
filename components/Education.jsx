391dc2de0da89d9bd48d69583278c9e6df19ac4f
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2022-12-21 13:04:47 -0600

    created content for education section with logo

--- components/Education.jsx
+++ components/Education.jsx
@@ -2,8 +2,20 @@ import React from 'react'
 
 const Training = () => {
   return (
-    <div>
-        
+    <div className='w-full md:h-screen p-2 flex items-center py-16'>
+        <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+          <div className='col-span-2'>
+            <p>Education</p>
+            <h2>Codeup</h2>
+            <p>Jul 2020 - Jan 2023</p>
+            <p>
+                A Fully immersive, project-based, and intensive 20-week program that provides individuals with 670 hours of expert instruction and mentorship in software development.
+            </p>
+          </div>
+          <div>
+            <img src="assets/CodeupLogo.png" alt="codeup logo" />
+          </div>
+        </div>
     </div>
   )
 }
