ff1cae4a74a813a1741e9e547cb89805646341b2
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2022-12-31 14:25:46 -0600

    added photo and button to main section

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,17 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import { AiOutlineMail } from "react-icons/ai";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import Typewriter from "typewriter-effect";
 
 function Main() {
   return (
-    <div id="home" className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div>
-          <h1 className="py-4 text-gray-700">
-            {`Hi, I'm`} <span className="text-[#2980b9]">Greg Rodriguez</span>
-          </h1>
-          <h1 className="py-4 text-gray-700">
+    <div id="home" className="pt-24 md:py-40 lg:py-60">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8 md:p-3">
+
+        {/* Column 1 */}
+        <div className="col-span-1 m-auto">
+          <Image
+            src="/assets/headshot.jpeg"
+            width={250}
+            height={250}
+            className="rounded-xl m-auto"
+          />
+        </div>
+
+        {/* Colunn 2 */}
+        <div className="col-span-2">
+          <h2 className="py-4 text-gray-700 text-center">
+            {`Hi, I'm`}{" "}
+            <span className="text-[#2980b9] text-5xl">Greg Rodriguez</span>
+          </h2>
+          <h2 className="py-4 text-gray-700 text-center">
             <Typewriter
               options={{
                 strings: [
@@ -24,8 +37,8 @@ function Main() {
                 pauseFor: 4500,
               }}
             />
-          </h1>
-          <p className="py-4 text-gray-600 md:max-w-[70%] m-auto">
+          </h2>
+          <p className="p-4 text-gray-600 m-auto">
             Self-Discipline Software Developer and Veteran with sales,
             problem-solving, and leadership experience. Being an adaptable
             individual with a passion to learn inspired the shift to focus on
@@ -33,7 +46,13 @@ function Main() {
             opportunity to leverage technical and collaborative skills to be
             part of a team and write clean code with innovative technologies.
           </p>
+          <div className="text-center">
+            <Link href="/#projects">
+              <button className="px-8 py-2 m-2">Projects</button>
+            </Link>
+          </div>
         </div>
+        
       </div>
     </div>
   );
