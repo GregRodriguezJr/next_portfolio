08aada23051f345597f1393003c6367b14f4a14a
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2022-12-31 16:45:33 -0600

    added a title to each project, deleted button on hover, made whole project div a link

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -1,21 +1,29 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const ProjectItem = ({title, projectImg, projectPage}) => {
+const ProjectItem = ({ title, projectImg, projectPage }) => {
   return (
-    <div className='relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#2980b9] to-[#2c3e50]'>
-        <Image className='rounded-xl group-hover:opacity-10' src={projectImg} alt='/'/>
-        <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-        <h3 className='text-2lx text-white tracking-wider text-center uppercase'>{title}</h3>
-        <Link href={projectPage}>
-            <p className='text-center m-5 p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>
+    <Link href={projectPage}>
+      <div className="hover:underline md:my-5">
+        <h3 className='text-center text-2xl mb-2 '>
+          {title}
+        </h3>
+        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#2980b9] to-[#2c3e50]">
+          <Image
+            className="rounded-xl group-hover:opacity-10"
+            src={projectImg}
+            alt="/"
+          />
+          <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <h3 className="text-2lx text-white tracking-wider text-center uppercase">
               More Info
-            </p>
-        </Link>
+            </h3>
+          </div>
         </div>
-    </div>
-  )
-}
+      </div>
+    </Link>
+  );
+};
 
-export default ProjectItem
+export default ProjectItem;
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
-import dashboardImg from '../public/assets/dashboard.png'
-import ProjectItem from './ProjectItem';
-import noelImg from '../public/assets/noelImg.png'
+import React from "react";
+import dashboardImg from "../public/assets/dashboard.png";
+import ProjectItem from "./ProjectItem";
+import noelImg from "../public/assets/noelImg.png";
 
 const Projects = () => {
   return (
-    <div id='projects' className='w-full pt-5'>
-        <div className='max-w-[1240px] mx-auto px-2 py-16'>
-          <p className='text-xl tracking-widest uppercase text-[#2980b9]'>Projects</p>
-          <h2 className='py-4'>{`What I've Built`}</h2>
-          <div className='grid md:grid-cols-2 gap-8'>
+    <div id="projects" className="w-full pt-5">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#2980b9]">
+          Projects
+        </p>
+        <h2 className="py-4">{`What I've Built`}</h2>
+        <div className="grid md:grid-cols-2 gap-8">
           <ProjectItem
             title="Crypto Dashboard"
             projectImg={dashboardImg}
-            projectPage='Dashboard'
+            projectPage="/Dashboard"
           />
           <ProjectItem
             title="Noel Designs"
             projectImg={noelImg}
-            projectPage='NoelDesigns'
+            projectPage="/NoelDesigns"
           />
-          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
