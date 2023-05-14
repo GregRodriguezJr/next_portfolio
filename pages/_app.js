c113887e8b2b78d003ec0076a455a70b5ab65046
Greg Rodriguez <86543125+GregRodriguezJr@users.noreply.github.com>
2023-05-14 10:39:53 -0500

    added framer-monition package, created animation on page load

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,21 @@
-import '../styles/globals.css'
-import NavBar from '../components/NavBar.jsx'
+import "../styles/globals.css";
+import NavBar from "../components/NavBar.jsx";
+import { motion, AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <NavBar />
-      <Component {...pageProps} />
-    </>
-  )
+    return (
+        <>
+            <NavBar />
+            <AnimatePresence>
+                <motion.div
+                    initial={{ opacity: 0, y: 15 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 15 }}
+                    transition={{ delay: 0.25 }}
+                >
+                    <Component {...pageProps} />
+                </motion.div>
+            </AnimatePresence>
+        </>
+    );
 }
